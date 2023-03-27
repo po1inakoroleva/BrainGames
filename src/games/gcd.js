47f12f722752ec881114ee59c923b66f269ba5f5
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-03-27 23:55:53 +0700

    add lodash

---
+++ src/games/gcd.js
@@ -0,0 +1,35 @@
+import gameEngine from '../index.js';
+
+const rulesOfTheGcdGame = 'Find the greatest common divisor of given numbers.';
+
+const roundGcdGame = () => {
+  const result = [];
+
+  const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const firstNum = getRandomNum(0, 100);
+  const secondNum = getRandomNum(0, 100);
+
+  const question = `${firstNum} ${secondNum}`;
+
+  const getDivisors = (num) => {
+    const divisors = [];
+    for (let divisor = 1; divisor <= num / 2; divisor += 1) {
+      if (num % divisor === 0) {
+        divisors.push(divisor);
+      }
+    }
+    return divisors;
+  };
+
+  const firstNumDivisors = getDivisors(firstNum);
+  const secondNumDivisors = getDivisors(secondNum);
+
+  const getGreatestDivisor = (divisors1, divisors2) => {};
+
+  result.push(question);
+  return result;
+};
+
+export default () => {
+  gameEngine(rulesOfTheGcdGame, roundGcdGame);
+};
