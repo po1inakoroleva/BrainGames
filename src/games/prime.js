2f67c360fa80adffafcc67c6e44ce2391ab77e64
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-03-29 19:22:04 +0700

    add  brain-prime.js and prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import gameEngine from '../index.js';
+
+const rulesOfThePrimeGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const roundPrimeGame = () => {
+  const result = [];
+  const geterateRandomInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const question = geterateRandomInRange(1, 100);
+
+  const isPrime = (num) => {
+    for (let divisor = 2; divisor <= num / 2; divisor += 1) {
+      if (num % divisor === 0) {
+        return 'no';
+      }
+    }
+    return 'yes';
+  };
+
+  const correctAnswer = isPrime(question);
+
+  result.push(question);
+  result.push(correctAnswer);
+  return result;
+};
+
+export default () => {
+  gameEngine(rulesOfThePrimeGame, roundPrimeGame);
+};
